6dfd4615129e6642c2618b6e9843b95d50ba1ab6
chetan <chetanpt@users.noreply.github.com>
2020-11-24 18:00:16 +0900

    mid way to excersise 3-9
    - motorcontroller.slx has model for 3-9
    - other files are updated with minor correction
    - some support files/models are created for test purspose with name prefix as test*

---
+++ PartThree/ControllerModel/ControllerModel_2020.m
@@ -0,0 +1,17 @@
+clear;
+close all;
+clc;
+
+% system parameter
+SV = 1000;
+Kp = 0.1;
+Ki = 0.1;
+Kd = 0;
+n = 8; 
+
+% simulation parameters
+Endtime = 10;
+
+filename = "motorcontroller.slx";
+open(filename);
+sim(filename);
