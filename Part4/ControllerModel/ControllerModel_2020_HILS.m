af100ef349d1a769cb335390084671d4e88dd8cc
chetan <chetanpt@users.noreply.github.com>
2020-11-25 14:59:47 +0900

    minor rearrangement of the source files. and model for part 4
    all the required material until part 4_4

---
+++ Part4/ControllerModel/ControllerModel_2020_HILS.m
@@ -0,0 +1,17 @@
+clear;
+close all;
+clc;
+
+% system parameter
+SV = 1000;
+Kp = 0.1;
+Ki = 0.1;
+Kd = 0;
+n = 10; 
+
+% simulation parameters
+Endtime = Inf; % simulation end time
+
+filename = "ControllerModel_sim_2020_HILS.slx";
+open(filename);
+%sim(filename);
---
+++ Part4/HILSimulator/HILSimulater_2016a.m
@@ -0,0 +1,8 @@
+%HILシミュレータ（HILシミュレータ用）
+clear
+close all
+clc
+
+%------------ シミュレーションの実行 ------------
+filename = 'HILSimulater_sim_2016a';    %Simulinkファイル名
+open(filename)                          %Simulinkオープン
